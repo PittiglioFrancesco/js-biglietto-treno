af154bc551c83e72298f44927395784cfd26ed00
PittiglioFrancesco <francescopittiglio79@gmail.com>
2021-05-27 14:58:42 +0200

    fine

--- js/functions.js
+++ js/functions.js
@@ -14,3 +14,26 @@ console.log(km)
 
 var age = prompt('Inserisci la tua età');
 console.log(age)
+
+var pSconto;
+
+// operazioni
+
+var costo = km * 0.21;
+
+if (age < 18) {
+
+    pSconto = Math.round(costo * 0.8);
+    document.getElementById("risultato").innerHTML = "Il prezzo del biglietto è di " + costo + "€, ma dato che lei è un under 18 ha diritto ad uno sconto del 20% che le farà pagare il biglietto solo " + pSconto + "€."
+
+} else if (age > 65) {
+
+    pSconto = Math.round(costo * 0.6);
+    document.getElementById("risultato").innerHTML = "Il prezzo del biglietto è di " + costo + "€, ma dato che lei è un over 65 ha diritto ad uno sconto del 40% che le farà pagare il biglietto solo " + pSconto + "€."
+
+
+} else {
+
+    document.getElementById("risultato").innerHTML = "Il prezzo del biglietto è di " + costo + "€.";
+
+}
